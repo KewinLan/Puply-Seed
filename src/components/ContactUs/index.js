0a027a07bfcd129e0e7a3e24a7952240cc43b3b0
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-31 16:01:51 -0400

    the website now works at 100% zoom. Add a cursor pointer to the buttons, then clean script

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -8,13 +8,22 @@ export default function ContactUs() {
         <h1>Contact Us</h1>
       </div>
       <div className='contact-address'>
-        <FaMapPin /><p>Pulpy Seed Offices<br></br>234 Somewhere Street<br></br>Toronto, ON M5H 2N2<br></br>Canada</p>
+        <div className='contact-icons'>
+          <FaMapPin />
+        </div>
+        <p>Pulpy Seed Offices<br></br>234 Somewhere Street<br></br>Toronto, ON M5H 2N2<br></br>Canada</p>
       </div>
       <div className='contact-phone'>
-        <FaPhone /><p>555-0100</p>
+        <div className='contact-icons'>
+          <FaPhone />
+        </div>
+        <p>555-0100</p>
       </div>
       <div className='contact-email'>
-        <FaAt /><p>pulpyseed@example.com</p>
+        <div className='contact-icons'>
+          <FaAt />
+        </div>
+        <p>pulpyseed@example.com</p>
       </div>
       <div className='contact-bg'></div>
     </>
